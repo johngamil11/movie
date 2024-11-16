f607792512f8b9ef96c5e66f8b44839440912feb
KimoStore <johnnygamil10@gmail.com>
2024-11-16 10:50:27 +0200

    finished main layout .

--- lib/core/utils/assets_image.dart
+++ lib/core/utils/assets_image.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
 
+class AssetsImage {
+  static const String homeIcon = 'assets/images/Home_icon.png' ;
+  static const String browseIcon = 'assets/images/browse_icon.png' ;
+  static const String searchIcon = 'assets/images/search_icon.png' ;
+  static const String watchlistIcon = 'assets/images/watchlist_icon.png' ;
+}
